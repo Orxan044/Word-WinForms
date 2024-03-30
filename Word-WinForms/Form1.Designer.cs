85b41e9a673dfce10a65a0c279f21557521d2a2e
Orxan Huseynov <115917125+Orxan044@users.noreply.github.com>
2024-03-30 17:10:31 +0400

    :

--- Word-WinForms/Form1.Designer.cs
+++ Word-WinForms/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             WordTable = new RichTextBox();
             label1 = new Label();
             ComboBoxFont = new ComboBox();
@@ -47,9 +48,10 @@
             // 
             // WordTable
             // 
-            WordTable.Location = new Point(0, 66);
+            WordTable.Location = new Point(0, 88);
+            WordTable.Margin = new Padding(3, 4, 3, 4);
             WordTable.Name = "WordTable";
-            WordTable.Size = new Size(800, 383);
+            WordTable.Size = new Size(914, 509);
             WordTable.TabIndex = 1;
             WordTable.Text = "";
             // 
@@ -57,18 +59,19 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(48, 9);
+            label1.Location = new Point(55, 12);
             label1.Name = "label1";
-            label1.Size = new Size(53, 25);
+            label1.Size = new Size(65, 32);
             label1.TabIndex = 2;
             label1.Text = "Font";
             // 
             // ComboBoxFont
             // 
             ComboBoxFont.FormattingEnabled = true;
-            ComboBoxFont.Location = new Point(12, 37);
+            ComboBoxFont.Location = new Point(14, 49);
+            ComboBoxFont.Margin = new Padding(3, 4, 3, 4);
             ComboBoxFont.Name = "ComboBoxFont";
-            ComboBoxFont.Size = new Size(121, 23);
+            ComboBoxFont.Size = new Size(138, 28);
             ComboBoxFont.TabIndex = 3;
             ComboBoxFont.Text = "Segoe UI";
             ComboBoxFont.SelectedIndexChanged += ComboBoxFont_SelectedIndexChanged;
@@ -77,18 +80,19 @@
             // 
             lblSize.AutoSize = true;
             lblSize.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            lblSize.Location = new Point(151, 9);
+            lblSize.Location = new Point(173, 12);
             lblSize.Name = "lblSize";
-            lblSize.Size = new Size(47, 25);
+            lblSize.Size = new Size(59, 32);
             lblSize.TabIndex = 4;
             lblSize.Text = "Size";
             // 
             // ComboBoxSize
             // 
             ComboBoxSize.FormattingEnabled = true;
-            ComboBoxSize.Location = new Point(142, 37);
+            ComboBoxSize.Location = new Point(162, 49);
+            ComboBoxSize.Margin = new Padding(3, 4, 3, 4);
             ComboBoxSize.Name = "ComboBoxSize";
-            ComboBoxSize.Size = new Size(59, 23);
+            ComboBoxSize.Size = new Size(67, 28);
             ComboBoxSize.TabIndex = 5;
             ComboBoxSize.Text = "9";
             ComboBoxSize.SelectedIndexChanged += ComboBoxSize_SelectedIndexChanged;
@@ -97,9 +101,9 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.Location = new Point(227, 9);
+            label2.Location = new Point(259, 12);
             label2.Name = "label2";
-            label2.Size = new Size(96, 25);
+            label2.Size = new Size(119, 32);
             label2.TabIndex = 6;
             label2.Text = "FontStyle";
             // 
@@ -108,96 +112,110 @@
             btnBold.BackColor = Color.White;
             btnBold.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
             btnBold.ForeColor = Color.Black;
-            btnBold.Location = new Point(227, 37);
+            btnBold.Location = new Point(259, 49);
+            btnBold.Margin = new Padding(3, 4, 3, 4);
             btnBold.Name = "btnBold";
-            btnBold.Size = new Size(31, 23);
+            btnBold.Size = new Size(35, 31);
             btnBold.TabIndex = 7;
             btnBold.Text = "B";
             btnBold.TextAlign = ContentAlignment.TopCenter;
             btnBold.UseVisualStyleBackColor = false;
-            btnBold.Click += btnB_Click;
+            btnBold.Click += btnBold_Click;
             // 
             // BtnUnderline
             // 
             BtnUnderline.BackColor = Color.White;
             BtnUnderline.Font = new Font("Segoe UI Light", 9.75F, FontStyle.Underline, GraphicsUnit.Point);
-            BtnUnderline.Location = new Point(260, 37);
+            BtnUnderline.Location = new Point(297, 49);
+            BtnUnderline.Margin = new Padding(3, 4, 3, 4);
             BtnUnderline.Name = "BtnUnderline";
-            BtnUnderline.Size = new Size(31, 23);
+            BtnUnderline.Size = new Size(35, 31);
             BtnUnderline.TabIndex = 8;
             BtnUnderline.Text = "U";
             BtnUnderline.UseVisualStyleBackColor = false;
+            BtnUnderline.Click += BtnUnderline_Click;
             // 
             // BtnItalic
             // 
             BtnItalic.BackColor = Color.White;
-            BtnItalic.Font = new Font("Segoe UI Light", 9.75F, FontStyle.Underline, GraphicsUnit.Point);
-            BtnItalic.Location = new Point(292, 37);
+            BtnItalic.Font = new Font("Consolas", 10.2F, FontStyle.Italic, GraphicsUnit.Point);
+            BtnItalic.Location = new Point(334, 49);
+            BtnItalic.Margin = new Padding(3, 4, 3, 4);
             BtnItalic.Name = "BtnItalic";
-            BtnItalic.Size = new Size(31, 23);
+            BtnItalic.Size = new Size(35, 31);
             BtnItalic.TabIndex = 9;
-            BtnItalic.Text = "/";
+            BtnItalic.Text = "|";
             BtnItalic.UseVisualStyleBackColor = false;
+            BtnItalic.Click += BtnItalic_Click;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            label3.Location = new Point(372, 9);
+            label3.Location = new Point(425, 12);
             label3.Name = "label3";
-            label3.Size = new Size(61, 25);
+            label3.Size = new Size(76, 32);
             label3.TabIndex = 10;
             label3.Text = "Color";
             // 
             // ComboBoxColor
             // 
             ComboBoxColor.FormattingEnabled = true;
-            ComboBoxColor.Location = new Point(342, 37);
+            ComboBoxColor.Location = new Point(391, 49);
+            ComboBoxColor.Margin = new Padding(3, 4, 3, 4);
             ComboBoxColor.Name = "ComboBoxColor";
-            ComboBoxColor.Size = new Size(121, 23);
+            ComboBoxColor.Size = new Size(138, 28);
             ComboBoxColor.TabIndex = 11;
             ComboBoxColor.Text = "Black";
             ComboBoxColor.SelectedIndexChanged += ComboBoxColor_SelectedIndexChanged;
             // 
             // TxtBoxLoad
             // 
-            TxtBoxLoad.Location = new Point(477, 11);
+            TxtBoxLoad.Location = new Point(545, 15);
+            TxtBoxLoad.Margin = new Padding(3, 4, 3, 4);
             TxtBoxLoad.Name = "TxtBoxLoad";
-            TxtBoxLoad.Size = new Size(219, 23);
+            TxtBoxLoad.PlaceholderText = "Enter Name and load file";
+            TxtBoxLoad.Size = new Size(250, 27);
             TxtBoxLoad.TabIndex = 12;
             // 
             // BtnLoad
             // 
             BtnLoad.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            BtnLoad.Location = new Point(702, 9);
+            BtnLoad.Location = new Point(802, 12);
+            BtnLoad.Margin = new Padding(3, 4, 3, 4);
             BtnLoad.Name = "BtnLoad";
-            BtnLoad.Size = new Size(86, 25);
+            BtnLoad.Size = new Size(98, 33);
             BtnLoad.TabIndex = 13;
             BtnLoad.Text = "Load";
             BtnLoad.UseVisualStyleBackColor = true;
+            BtnLoad.Click += BtnLoad_Click;
             // 
             // TxtBoxSave
             // 
-            TxtBoxSave.Location = new Point(477, 37);
+            TxtBoxSave.Location = new Point(545, 49);
+            TxtBoxSave.Margin = new Padding(3, 4, 3, 4);
             TxtBoxSave.Name = "TxtBoxSave";
-            TxtBoxSave.Size = new Size(219, 23);
+            TxtBoxSave.PlaceholderText = "Tap to enter name (.txt .json)";
+            TxtBoxSave.Size = new Size(250, 27);
             TxtBoxSave.TabIndex = 14;
             // 
             // BtnSave
             // 
             BtnSave.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            BtnSave.Location = new Point(702, 37);
+            BtnSave.Location = new Point(802, 49);
+            BtnSave.Margin = new Padding(3, 4, 3, 4);
             BtnSave.Name = "BtnSave";
-            BtnSave.Size = new Size(86, 25);
+            BtnSave.Size = new Size(98, 33);
             BtnSave.TabIndex = 15;
             BtnSave.Text = "Save";
             BtnSave.UseVisualStyleBackColor = true;
+            BtnSave.Click += BtnSave_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
             Controls.Add(BtnSave);
             Controls.Add(TxtBoxSave);
             Controls.Add(BtnLoad);
@@ -213,6 +231,8 @@
             Controls.Add(ComboBoxFont);
             Controls.Add(label1);
             Controls.Add(WordTable);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Word";
             ResumeLayout(false);
--- Word-WinForms/Form1.cs
+++ Word-WinForms/Form1.cs
@@ -1,69 +1,101 @@
-namespace Word_WinForms
+using System.Text.RegularExpressions;
+
+namespace Word_WinForms;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+
+    public Form1()
+    {
+        InitializeComponent();
+        foreach (FontFamily font in FontFamily.Families)
+            ComboBoxFont.Items.Add(font.Name);
+
+        for (int i = 8; i <= 72; i += 2)
+            ComboBoxSize.Items.Add(i);
+
+        foreach (KnownColor color in Enum.GetValues(typeof(KnownColor)))
+            if (!color.ToString().StartsWith("Inactive"))
+                ComboBoxColor.Items.Add(color);
+    }
+
+    string? selectedFontName;
+    int selectedSize = 9;
+    string? selectedColorName;
+
+    private void ComboBoxFont_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        selectedFontName = ComboBoxFont.SelectedItem.ToString();
+        Font newFont = new(selectedFontName, selectedSize);
+        WordTable.SelectionFont = newFont;
+    }
+    private void ComboBoxSize_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        selectedSize = Convert.ToInt32(ComboBoxSize.SelectedItem);
+        Font newFont = new(selectedFontName, selectedSize);
+        WordTable.SelectionFont = newFont;
+    }
+
+
+    private void btnBold_Click(object sender, EventArgs e)
     {
 
-        public Form1()
+        bool isBold = WordTable.SelectionFont.Bold;
+        Font selectionFont = WordTable.SelectionFont;
+        if (isBold) btnBold.BackColor = Color.White;
+        else btnBold.BackColor = Color.Gray;
+        if (!(selectionFont is null))
+            WordTable.SelectionFont = new(selectedColorName, selectedSize, selectionFont.Style ^ FontStyle.Bold);
+
+        // ^ bitwise operatorudur Eger Boldursa Oz Styleni qayidayacaq eger Bold deyilse Style Bold elave edecek
+
+    }
+    private void BtnUnderline_Click(object sender, EventArgs e)
+    {
+        bool isUnderline = WordTable.SelectionFont.Underline;
+        Font selectionFont = WordTable.SelectionFont;
+        if (isUnderline) BtnUnderline.BackColor = Color.White;
+        else BtnUnderline.BackColor = Color.Gray;
+        if (!(selectionFont is null))
+            WordTable.SelectionFont = new(selectedColorName, selectedSize, selectionFont.Style ^ FontStyle.Underline);
+    }
+    private void BtnItalic_Click(object sender, EventArgs e)
+    {
+        bool isItalic = WordTable.SelectionFont.Italic;
+        Font selectionFont = WordTable.SelectionFont;
+        if (isItalic) BtnItalic.BackColor = Color.White;
+        else BtnItalic.BackColor = Color.Gray;
+        if (!(selectionFont is null))
+            WordTable.SelectionFont = new(selectedColorName, selectedSize, selectionFont.Style ^ FontStyle.Italic);
+    }
+
+    private void ComboBoxColor_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        selectedColorName = ComboBoxColor.SelectedItem.ToString();
+        Color color = Color.FromName(selectedColorName);
+        WordTable.SelectionColor = color;
+    }
+
+    private void BtnLoad_Click(object sender, EventArgs e)
+    {
+        string? fileName = $@"{TxtBoxLoad.Text}";
+
+        if (File.Exists(fileName))
+            WordTable.Text = File.ReadAllText(fileName);
+        else
+            MessageBox.Show("Not Found File", "Eror Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+    }
+
+    private void BtnSave_Click(object sender, EventArgs e)
+    {
+        string? fileName = TxtBoxSave.Text;
+        string? regexFileName = @"(\.json|\.txt)$";
+        if(Regex.IsMatch(fileName, regexFileName))
         {
-            InitializeComponent();
-            foreach (FontFamily font in FontFamily.Families)
-                ComboBoxFont.Items.Add(font.Name);
-
-            for (int i = 8; i <= 72; i += 2)
-                ComboBoxSize.Items.Add(i);
-
-            foreach (KnownColor color in Enum.GetValues(typeof(KnownColor)))
-                if (!color.ToString().StartsWith("Inactive"))
-                    ComboBoxColor.Items.Add(color);
+            File.WriteAllText(fileName, WordTable.Text);
+            MessageBox.Show("File Name is not correct", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
-
-        string? selectedColorName;
-        int selectedSize;
-        string? selectedFontName;
-
-        private void btnB_Click(object sender, EventArgs e)
-        {
-
-            bool isBold = WordTable.SelectionFont.Bold;
-
-            if (isBold)
-            {
-                btnBold.BackColor = Color.White;
-                //WordTable.SelectionFont = new(selectedColorName, selectedSize, WordTable.Font.Style);
-                //WordTable.SelectionFont = new(selectedColorName, selectedSize, FontStyle.Italic);
-                //WordTable.SelectionFont = new(selectedColorName, selectedSize, FontStyle.Underline);
-            }
-            else
-            {
-                btnBold.BackColor = Color.Gray;
-                WordTable.SelectionFont = new(selectedColorName, selectedSize, FontStyle.Bold);
-                //WordTable.SelectionFont = new(selectedColorName, selectedSize, FontStyle.Italic);
-                //WordTable.SelectionFont = new(selectedColorName, selectedSize, FontStyle.Underline);
-            }
-
-
-        }
-
-        private void ComboBoxFont_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            selectedFontName = ComboBoxFont.SelectedItem.ToString();
-            Font newFont = new(selectedFontName, selectedSize);
-            WordTable.SelectionFont = newFont;
-        }
-        private void ComboBoxSize_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            selectedSize = Convert.ToInt32(ComboBoxSize.SelectedItem);
-            Font newFont = new(selectedFontName, selectedSize);
-            WordTable.SelectionFont = newFont;
-        }
-
-        private void ComboBoxColor_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            selectedColorName = ComboBoxColor.SelectedItem.ToString();
-            Color color = Color.FromName(selectedColorName);
-            WordTable.SelectionColor = color;
-        }
-
-
+        else
+            MessageBox.Show("File Name is not correct", "Eror Message", MessageBoxButtons.OK, MessageBoxIcon.Warning);
     }
 }
